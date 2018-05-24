930e35bd23c2badcb7b6c5dd215fa5a780897f11
Nahom <0953357@hr.nl>
2018-05-24 11:32:59 +0200

    Code

---
+++ Project 4/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Content;
+
+namespace Project_4
+{
+    [Activity(Label = "Project_4", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Hoofdmenu);
+
+            var imageButton1 = FindViewById<ImageButton>(Resource.Id.imageButton1);
+            var imageButton2 = FindViewById<ImageButton>(Resource.Id.imageButton2);
+            var imageButton3 = FindViewById<ImageButton>(Resource.Id.imageButton3);
+
+            imageButton1.Click += delegate
+            {
+                Console.WriteLine("CLICKED imageButton1");
+                Intent nextActivity = new Intent(this, typeof(Thuisoefeningen));
+                StartActivity(nextActivity);
+
+            };
+
+            imageButton2.Click += delegate
+            {
+                Console.WriteLine("CLICKED imageButton2");
+                Intent nextActivity = new Intent(this, typeof(BMI));
+                StartActivity(nextActivity);
+
+            };
+
+            imageButton3.Click += delegate
+            {
+                Console.WriteLine("CLICKED imageButton3");
+                Intent nextActivity = new Intent(this, typeof(Timer));
+                StartActivity(nextActivity);
+
+            };
+        }
+    }
+}
+
---
+++ Project 4/Thuisoefeningen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Project_4
+{
+    [Activity(Label = "Thuisoefeningen")]
+    public class Thuisoefeningen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Thuisoefeningen);
+            // Create your application here
+            var imageButton1 = FindViewById<ImageButton>(Resource.Id.imageButton1);
+            var imageButton2 = FindViewById<ImageButton>(Resource.Id.imageButton2);
+            var imageButton3 = FindViewById<ImageButton>(Resource.Id.imageButton3);
+            var imageButton4 = FindViewById<ImageButton>(Resource.Id.imageButton4);
+            var imageButton5 = FindViewById<ImageButton>(Resource.Id.imageButton5);
+
+        }
+    }
+}
