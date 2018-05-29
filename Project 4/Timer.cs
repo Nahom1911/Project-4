7a89e804f667a11d9a7dbf74f49203deec112eb2
Nahom <0953357@hr.nl>
2018-05-29 12:25:16 +0200

    Test
    test

--- Project 4/Timer.cs
+++ Project 4/Timer.cs
@@ -9,17 +9,67 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RadialProgress;
+using System.Timers;
 
 namespace Project_4
 {
     [Activity(Label = "Timer")]
     public class Timer : Activity
     {
+        RadialProgressView radialProgressView;
+        Button btnStart , btnStop;
+        TextView txtTimer;
+
+        Timer timer;
+
+        int hour = 0, min = 0, sec = 0;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
+            SetContentView(Resource.Layout.Timer);
+
+           /* radialProgressView = FindViewById<RadialProgressView>(Resource.Id.progressView);
+            btnStart = FindViewById<Button>(Resource.Id.btnStart);
+            btnStop = FindViewById<Button>(Resource.Id.btnStop);
+            txtTimer = FindViewById<TextView>(Resource.Id.txtTimer);
+
+            btnStart.Click += delegate {
+               // btnStart.Enabled = false;
+               // btnStop.Enabled = true;
+                timer = new Timer();
+                timer.Interval = 1000;
+                timer.Elapsed += Timer_Elapsed;
+                timer.Start();
+            };
+
+            btnStop.Click += delegate {
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                timer.Dispose();
+                timer = null;
+
+            };
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            sec++;
+            if (sec == 60)
+            {
+                min++;
+                sec = 0;
+            }
+            if (min == 60)
+            {
+                hour++;
+                min = 0;
+            }
+            RunOnUiThread(() => { txtTimer.Text = $"{hour}:{min}:{sec}"; });
+            radialProgressView.Value = sec;*/
         }
     }
 }
--- Project 4/obj/Debug/android/src/project_4/project_4/R.java
+++ Project 4/obj/Debug/android/src/project_4/project_4/R.java
@@ -21,18 +21,24 @@ public final class R {
         public static final int rug_8=0x7f020007;
         public static final int test_1=0x7f020008;
         public static final int thuisoefeningen_9=0x7f020009;
+        public static final int timer_3=0x7f02000a;
     }
     public static final class id {
+        public static final int btnStart=0x7f050007;
+        public static final int btnStop=0x7f050008;
         public static final int imageButton1=0x7f050000;
         public static final int imageButton2=0x7f050001;
         public static final int imageButton3=0x7f050002;
         public static final int imageButton4=0x7f050004;
         public static final int imageButton5=0x7f050003;
+        public static final int progressView=0x7f050005;
+        public static final int txtTimer=0x7f050006;
     }
     public static final class layout {
         public static final int bmi=0x7f030000;
         public static final int hoofdmenu=0x7f030001;
         public static final int thuisoefeningen=0x7f030002;
+        public static final int timer=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
